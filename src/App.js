9c88cd8663d4ae91ed237c6bbaba5b34e1d7ce80
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-03-21 19:41:12 +0200

    finished setting up the project's routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
 
 import Homepage from "./pages/Homepage";
 
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div>
-      <Homepage />
+      <Routes>
+        <Route path="/" Component={Homepage} />
+        <Route path="/hats" Component={HatsPage} />
+      </Routes>
     </div>
   );
 }
--- src/components/menu-item/MenuItem.jsx
+++ src/components/menu-item/MenuItem.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 // styles
 import "./menu-item.scss";
 
@@ -13,7 +15,9 @@ const MenuItem = ({ title, imageUrl, size }) => (
     />
     <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="subtitle">SHOP NOW</span>
+      <span className="subtitle">
+        <Link to="/hats">SHOP NOW</Link>
+      </span>
     </div>
   </div>
 );
